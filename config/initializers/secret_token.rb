b3e6338341e77033359e2cc88118fe750e9bb1fb
paulbhartzog <PaulBHartzog@panarchy.com>
2011-01-07 17:02:54 -0500

    first push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flowwork::Application.config.secret_token = '7ff9a1e9d1d6f7da98bc664ba913e3b1f65247b1faaa7197bf1bbdd453f6386f2e8c3bf2dbdfb4bfc4d746331d7a0ce95efa0cd90992f283c647713c3b3e498a'
