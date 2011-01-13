c0e6e1324b951615bfdf876a32135f8a6b847414
paulbhartzog <PaulBHartzog@panarchy.com>
2011-01-13 10:49:15 -0500

    added sammy js routing framework

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,19 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+// Sammy
+var appState = function(state){
+    if(state=="sammy"){
+        alert('sammy is working');
+    }
+};
+(function($) {
+    var app = $.sammy(function() {
+        this.get('#/sammy', function() {
+            appState("sammy");
+        });
+    });
+    $(function() {
+        app.run()
+    });
+})(jQuery);
