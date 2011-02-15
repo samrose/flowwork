97e7b35ad15efe88604e655ab9436badb8fcf497
Sam Rose <samuel.rose@gmail.com>
2011-02-15 15:44:48 -0500

    adding view for quora

--- app/controllers/quora_controller.rb
+++ app/controllers/quora_controller.rb
@@ -1,4 +1,14 @@
+require 'nokogiri'
+require 'open-uri'
+
 class QuoraController < ApplicationController
 	def index
+
+	#url = "http://www.walmart.com/search/search-ng.do?search_constraint=0&ic=48_0&search_query=batman&Find.x=0&Find.y=0&Find=Find"
+	url = "http://www.quora.com/What-are-the-fundamental-requirements-and-building-blocks-of-a-distributed-internet"
+	doc = Nokogiri::HTML(open(url))
+
+	quora = puts doc.xpath('//span')
+
 	end
 end
